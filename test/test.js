db2fd0949575445604d7cd9e2006e9f4df47bb3d
Rohan Prasad <roranj@gmail.com>
2017-07-02 19:17:30 -0400

    Add tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const functions = require('../basic-functions');
+
+
+describe('Test Hello Name function', function() {
+  it('returns Hello Test', function() {
+    expect('Hello Test', functions.helloName('Test'));
+  });
+});
