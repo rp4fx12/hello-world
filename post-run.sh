82f40000c461aed8cfec6dd3384667cb5e1807a0
Rohan Prasad <roranj@gmail.com>
2017-07-05 15:06:35 -0400

    Fix shell script'

--- post-run.sh
+++ post-run.sh
@@ -1,10 +1,9 @@
 #!bin/bash
-sleep 5m
+sleep 4m
 aws s3 cp s3://hello-world-rohan/current/instance.txt current.txt
 aws s3 rm s3://hello-world-rohan/current/instance.txt
 export CURRENT=`cat current.txt`
-aws ec2 terminate-instances --instance-ids $CURRENT
-aws s3 cp s3://hello-world-rohan/new/instance.txt
-export NEW=`cat new.txt`
+aws ec2 terminate-instances --instance-ids $CURRENT --region us-east-1
+aws s3 cp s3://hello-world-rohan/new/instance.txt new.txt
 aws s3 rm s3://hello-world-rohan/new/instance.txt
 aws s3 cp new.txt s3://hello-world-rohan/current/instance.txt
