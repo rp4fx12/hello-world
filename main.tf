e767202bb46e2c9e9c1276c90d4722e2a6645c1d
Rohan Prasad <roranj@gmail.com>
2017-07-05 15:16:32 -0400

    Remove commented out code

--- main.tf
+++ main.tf
@@ -23,19 +23,6 @@ resource "aws_instance" "example" {
               cd /app/hello-world
               npm install
               npm start
-              #chmod 755 /tmp
-              #cd /tmp
-              #echo `aws s3 ls s3://hello-world-rohan/current/ --recursive | tail -n 1` | awk '{print $4}' | sed 's/^.\{8\}//g' >> currentFile.txt
-              #export CURRENT_FILE=`cat currentFile.txt`
-              #aws s3 cp s3://hello-world-rohan/current/instance.txt current.txt
-              #aws s3 rm s3://hello-world-rohan/current/instance.txt
-              #export CURRENT=`cat current.txt`
-              #aws ec2 terminate-instances --instance-ids $CURRENT
-              #echo `aws s3 ls s3://hello-world-rohan/current/ --recursive | tail -n 1` | awk '{print $4}' | sed 's/^.\{4\}//g' >> new.txt
-              #aws s3 cp s3://hello-world-rohan/new/instance.txt
-              #export NEW=`cat new.txt`
-              #aws s3 rm s3://hello-world-rohan/new/instance.txt
-              #aws s3 cp new.txt s3://hello-world-rohan/current/instance.txt
               EOF
 
   tags {
